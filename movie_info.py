1a5335b6b9c3619970fc027f64c5c8cb5a10ea5f
Grace Durham <gracelee.durham@gmail.com>
2016-09-07 21:13:15 -0700

    Add files via upload

--- movie_info.py
+++ movie_info.py
@@ -1,4 +1,3 @@
-
 class Movie_info(object): 
 	def __init__(self, director="",release_year="",title="",actor_1="",actor_2="",location=""):
 		self.director=director 
@@ -8,7 +7,6 @@ class Movie_info(object):
 		self.actor_2=actor_2
 		self.location=location 
 
-movie1 = Movie_info("James Cameron", "1999", "Titanic", "Leonardo DiCaprio", "Kate Winslet", "Atlantic Ocean")
+# movie1 = Movie_info("James Cameron", "1999", "Titanic", "Leonardo DiCaprio", "Kate Winslet", "Atlantic Ocean")
 
-print movie1.__dict__
-
+# print movie1.__dict__
